20629aeaca14e69535ea0daf735c06fc872f1dfe
HappyFurbFarms <94417030+HappyFurbFarms@users.noreply.github.com>
2023-01-30 17:36:34 -0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 Hooks.once('diceSoNiceInit', (dice3d) => {
-    dice3d.addTexture("Beans", {
-        name: "Beans",
+    dice3d.addTexture("PS2 Majima", {
+        name: "PS2 Majima",
         composite: "source-over",
-        source: "modules/bean-dice/images/beans.png",
-        bump:"modules/bean-dice/images/beans.png"
+        source: "modules/PS2-Majima-Dice/images/majeng.png",
+        bump:"modules/PS2-Majima-Dice/images/majeng.png"
     });
 
 });
