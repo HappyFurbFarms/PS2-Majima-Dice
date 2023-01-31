6eb936af049899f248a5c192332abd3eceb86691
HappyFurbFarms <94417030+HappyFurbFarms@users.noreply.github.com>
2023-01-30 17:15:29 -0800

    Delete scripts directory

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-Hooks.once('diceSoNiceInit', (dice3d) => {
-    dice3d.addTexture("Beans", {
-        name: "Beans",
-        composite: "source-over",
-        source: "modules/bean-dice/images/beans.png",
-        bump:"modules/bean-dice/images/beans.png"
-    });
-
-});
